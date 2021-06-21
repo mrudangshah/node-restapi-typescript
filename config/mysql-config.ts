91bde785f90ee10b97c2ef5cf8a2d5da5416239b
Mrudang Shah <mrudang.shah@iflair.com>
2021-06-21 19:17:34 +0530

    update sql connecion

--- config/mysql-config.ts
+++ config/mysql-config.ts
@@ -1,6 +1,6 @@
-import * as mysql from 'mysql';
+const mysql = require('mysql')
+const util = require('util')
 import { ENV } from './config.json';
-import * as util from 'util';
 
 export var pool = mysql.createPool({
   host: ENV.DB_HOST,
@@ -9,4 +9,10 @@ export var pool = mysql.createPool({
   database: ENV.DB_NAME
 });
 
-exports.pool = pool;
+export const dbPool = {
+  query: (text: any) => util.promisify(pool.query),
+  pool: pool
+};
+
+pool.query = util.promisify(pool.query);
+pool.getConnection = util.promisify(pool.getConnection);
